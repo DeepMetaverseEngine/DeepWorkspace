9096522aa5541dc687d11022cbd649e7d3b9c947
wazazhang <wazazhang@outlook.com>
2020-04-29 16:36:04 +0800

    1

---
+++ DeepModules/DeepHttpService/HttpService.cs
@@ -0,0 +1,20 @@
+﻿using DeepCrystal.RPC;
+using System;
+using System.Threading.Tasks;
+
+namespace DeepHttpService
+{
+    public class HttpService : IService
+    {
+        public HttpService(ServiceStartInfo start) : base(start) { }
+        protected override void OnDisposed() { }
+        protected override async Task OnStartAsync()
+        {
+
+        }
+        protected override Task OnStopAsync(ServiceStopInfo reason)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
